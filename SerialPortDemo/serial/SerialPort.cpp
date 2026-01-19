75e2c82096da2c1524ff7a63a1255c53912d6008
ben.cheng <ben.cheng@robotrak.cn>
2026-01-19 10:55:22 +0800

    222

---
+++ SerialPortDemo/serial/SerialPort.cpp
@@ -0,0 +1,71 @@
+/*
+ * @Author: qi.wang (qi.wang@robotrak.cn)
+ * @Date: 2025-02-19 14:47:41
+ * @LastEditors: qi.wang (qi.wang@robotrak.cn)
+ * @LastEditTime: 2025-02-19 16:25:26
+ * @FilePath: \saris\saris\src\control\driver\serial\SerialPort.cpp
+ * @Description:
+ *
+ * Copyright (c) 2025 by Robotrak, All Rights Reserved.
+ */
+#include "SerialPort.h"
+#include <spdlog/spdlog.h>
+
+namespace {
+
+constexpr size_t buffer_size{ 1024 };
+
+} // namespace
+
+namespace saris::control {
+
+SerialPort::SerialPort(const std::string& com, SerialPortReceiver* rcv)
+    : serial(io, com), received(buffer_size), receiver(rcv)
+{
+    using asio::serial_port_base;
+
+    serial.set_option(serial_port_base::baud_rate(115200));
+    serial.set_option(serial_port_base::character_size(8));
+    serial.set_option(serial_port_base::parity(serial_port_base::parity::none));
+    serial.set_option(
+        serial_port_base::stop_bits(serial_port_base::stop_bits::one));
+
+    thread = std::jthread([this]() {
+        startRead();
+        io.run();
+    });
+}
+
+SerialPort::~SerialPort()
+{
+    serial.cancel();
+    io.restart();
+}
+
+void SerialPort::startRead()
+{
+    serial.async_read_some(
+        asio::buffer(received),
+        [this](const asio::error_code& error, std::size_t bytesTransferred) {
+            if (!error) {
+                receiver->onDataReceived({ received.data(), bytesTransferred });
+                startRead();
+            }
+            else {
+                spdlog::error("serial port read error: {}", error.message());
+            }
+        });
+}
+
+void SerialPort::write(std::span<const uint8_t> msg)
+{
+    serial.async_write_some(
+        asio::buffer(msg.data(), msg.size()),
+        [](const asio::error_code& error, std::size_t bytesTransferred) {
+            if (error) {
+                spdlog::error("serial port write error: {}", error.message());
+            }
+        });
+}
+
+} // namespace saris::control
---
+++ SerialPortDemo/serial/SerialPort.h
@@ -0,0 +1,43 @@
+/*
+ * @Author: qi.wang (qi.wang@robotrak.cn)
+ * @Date: 2025-02-19 14:47:45
+ * @LastEditors: qi.wang (qi.wang@robotrak.cn)
+ * @LastEditTime: 2025-02-21 09:35:02
+ * @FilePath: \saris\saris\src\control\driver\serial\SerialPort.h
+ * @Description:
+ *
+ * reference: 28电机驱动协议-V38.pdf
+ *
+ * Copyright (c) 2025 by Robotrak, All Rights Reserved.
+ */
+#pragma once
+
+#include <span>
+#include <vector>
+#include <string>
+#include <thread>
+#include <asio.hpp>
+
+namespace saris::control {
+
+struct SerialPortReceiver {
+    virtual void onDataReceived(std::span<uint8_t>) = 0;
+};
+
+class SerialPort {
+public:
+    SerialPort(const std::string& com, SerialPortReceiver* rcv);
+    ~SerialPort();
+    void write(std::span<const uint8_t> msg);
+
+private:
+    SerialPortReceiver* receiver{ nullptr };
+    std::vector<uint8_t> received;
+    asio::io_context io;
+    asio::serial_port serial;
+    std::jthread thread;
+
+    void startRead();
+};
+
+} // namespace saris::control
